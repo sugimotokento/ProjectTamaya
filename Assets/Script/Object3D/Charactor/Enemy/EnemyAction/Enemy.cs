2ebcac9820790acff389fa7f2eaf3fba2f9b53eb
nanasesakamoto <mknn797.gmail.com>
2021-11-26 02:18:10 +0900

    簡易経路探索完成

--- Assets/Script/Object3D/Charactor/Enemy/EnemyAction/Enemy.cs
+++ Assets/Script/Object3D/Charactor/Enemy/EnemyAction/Enemy.cs
@@ -20,6 +20,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int ItemNum;　　　　//持っているアイテムナンバー
     private float VigTime;                       //警戒時継続時間
     private float DanTime;                       //戦闘時継続時間
+    [SerializeField] private bool isHelp;                         //お呼ばれされたか
 
 
     //弾関連
@@ -48,8 +49,16 @@ public class Enemy : MonoBehaviour
 
 
     //探索関連
-    [SerializeField] private GameObject LocalPoints;//移動ポイント
-    private int index = 0;
+    [SerializeField] private GameObject LocalPoints; //通常時移動ポイント
+    [SerializeField] private GameObject GlobalPoints;//お呼ばれ時移動ポイント
+    private int Lindex = 0;
+    private int Gindex = 0;
+    private bool isStart;
+    private Vector3 Goal;
+    private int IndexStart;
+    private int IndexGoal;
+    private float KeepLenS;
+    private float KeepLenG;
 
     //UI関連
     private float AddAlert = 5.0f;//UI加算値
@@ -69,8 +78,14 @@ public class Enemy : MonoBehaviour
         DanPlayer = false;
         SeeRay = false;
         SeePlayer = false;
+        isHelp = false;
         VigTime = 0.0f;
         DanTime = 0.0f;
+        isStart = true;
+        IndexStart = 0;
+        IndexGoal = 0;
+        KeepLenS = 100.0f;
+        KeepLenG = 100.0f;
     }
 
 
@@ -81,7 +96,12 @@ public class Enemy : MonoBehaviour
 
         if (isSumaki == false)
         {
-            if (alert <= 0)
+            if (alert < 1 && isHelp == true)
+            {
+                HelpEnemy();
+            }
+
+            if (alert <= 0 && isHelp == false)
                 SearchStatus();
             else if (alert > 0 && alert < 100)
                 WarningStatus();
@@ -170,8 +190,7 @@ public class Enemy : MonoBehaviour
     private void FightStatus()
     {
         DanTime += Time.deltaTime;
-
-        Debug.Log(DanTime);
+        
         if (DanTime < 5.0f)
         {
             if (SeePlayer == true)
@@ -239,6 +258,80 @@ public class Enemy : MonoBehaviour
 
 
     //============================================================================
+    //お呼ばれ状態
+    //============================================================================
+    private void HelpEnemy()
+    {
+        if (isStart)
+        {
+            float length = 0.0f;
+            Vector3 Rvec, dist;
+            RaycastHit hit;
+            KeepLenS = 100;
+            KeepLenG = 100;
+            for (int i = 0; i< GlobalPoints.transform.childCount; i++)
+            {
+                dist = GlobalPoints.transform.GetChild(i).gameObject.transform.position;
+
+                //障害物の有無
+                Goal = player.gameObject.transform.position;
+                Rvec = Goal - dist;
+                length = Rvec.magnitude;
+                if (Physics.Raycast(dist, Rvec, out hit, length + 1.0f))
+                {
+                    if (hit.collider.gameObject.tag == "Player")
+                    {
+                        if(KeepLenS > length)
+                        {
+                            KeepLenS = length;
+                            IndexGoal = i;
+                        }
+                    }
+                }
+
+                Goal = transform.position;
+                Rvec = Goal - dist;
+                length = Rvec.magnitude;
+                if (Physics.Raycast(dist, Rvec, out hit, length + 1.0f))
+                {
+                    if (hit.collider.gameObject.tag == "Enemy")
+                    {
+                        if (KeepLenG > length)
+                        {
+                            KeepLenG = length;
+                            IndexStart = i;
+                        }
+                    }
+                }
+            }
+            Gindex = IndexStart;
+
+            isStart = false;
+        }
+        else
+        {
+            Vector3 dist = GlobalPoints.transform.GetChild(Gindex).gameObject.transform.position
+                - this.transform.position;
+            this.transform.position += dist.normalized * Time.deltaTime * Espeed * 1.2f;
+
+            if (dist.magnitude < 0.5f && (IndexStart > IndexGoal))
+            {
+
+                if (Gindex == IndexGoal) Gindex = IndexGoal;
+                else Gindex--;
+            }
+            else if(dist.magnitude < 0.5f && (IndexStart < IndexGoal))
+            {
+                if (Gindex == IndexGoal) Gindex = IndexGoal;
+                else Gindex++;
+            }
+
+            ViewEnemy();
+        }
+    }
+
+
+    //============================================================================
     //戦闘不能状態
     //============================================================================
     private void DieEnemy()
@@ -257,13 +350,13 @@ public class Enemy : MonoBehaviour
     //移動（探索時）
     private void SearchMove(float sp)
     {
-        Vector3 dist = LocalPoints.transform.GetChild(index).gameObject.transform.position - this.transform.position;
+        Vector3 dist = LocalPoints.transform.GetChild(Lindex).gameObject.transform.position - this.transform.position;
         this.transform.position += dist.normalized * Time.deltaTime * sp;
 
         if (dist.magnitude < 0.5f)
         {
-            index++;
-            if (index >= LocalPoints.transform.childCount) index = 0;
+            Lindex++;
+            if (Lindex >= LocalPoints.transform.childCount) Lindex = 0;
         }
     }
 
@@ -404,9 +497,6 @@ public class Enemy : MonoBehaviour
         //ゲージ減少
         if (!VigPlayer && !DanPlayer)
         {
-            //UIscript.AddAlertness(-1 * Time.deltaTime * AddAlert);
-
-
             UIscript.SetAlertness(0);
         }
     }
