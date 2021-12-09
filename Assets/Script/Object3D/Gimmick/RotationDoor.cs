e74af4bb2d411389adbbf2fc622b7d25bb3ba6e0
sugimotokento <keyunutoma@gmail.com>
2021-12-09 11:48:47 +0900

    セキュリティードアと回転ドアにインタラクトマークを追加

--- Assets/Script/Object3D/Gimmick/RotationDoor.cs
+++ Assets/Script/Object3D/Gimmick/RotationDoor.cs
@@ -6,14 +6,42 @@ public class RotationDoor : MonoBehaviour {
     private Player player;
 
     [HideInInspector] public bool isRotation = false;
+
     [SerializeField] private GameObject standPointUp;
     [SerializeField] private GameObject standPointDown;
+    [SerializeField] private GameObject interact;
+
     private GameObject standObj;
+
+    private const float INTERACT_INTERVAL = 3;
 
     private float startAngle;
     private float rotationLate = 0;
+    private float interactIntervalTimer = 0;
+
+    private bool isInputMouseDown = false;
+    private bool isInteractInterval = false;
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0) == true) {
+            isInputMouseDown = true;
+        }
+    }
 
     private void FixedUpdate() {
+        Rotation();
+
+        if (isInteractInterval == true) {
+            interactIntervalTimer += Time.fixedDeltaTime;
+            if (interactIntervalTimer > INTERACT_INTERVAL) {
+                isInteractInterval = false;
+            }
+        }
+
+        interact.transform.rotation = Quaternion.identity;
+    }
+
+    void Rotation() {
         if (isRotation == true) {
             float angle = Mathf.Lerp(startAngle, startAngle + 180, rotationLate);
             this.transform.eulerAngles = Vector3.forward * angle;
@@ -32,28 +60,51 @@ public class RotationDoor : MonoBehaviour {
         }
     }
 
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            isInputMouseDown = false;
+        }
+    }
+    private void OnTriggerStay(Collider other) {
+        if (isInteractInterval == true) isInputMouseDown = false;
 
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("Player")) {
-            player = collision.gameObject.GetComponent<Player>();
+        if (other.gameObject.CompareTag("Player")) {
+            if (isInputMouseDown == true && isRotation == false) {
+                player = other.gameObject.GetComponent<Player>();
 
-            //プレイヤーを移動出来なくする
-            player.RemoveAction<PlayerMoveAction>();
-            player.moveSpeed = Vector3.zero;
+                //プレイヤーを移動出来なくする
+                player.RemoveAction<PlayerMoveAction>();
+                player.moveSpeed = Vector3.zero;
 
-            Vector3 distUp = collision.gameObject.transform.position - standPointUp.transform.position;
-            Vector3 distDown = collision.gameObject.transform.position - standPointDown.transform.position;
-            //近い方の待機地点に移動する
-            if (distUp.magnitude > distDown.magnitude) {
-                collision.gameObject.transform.position = standPointDown.transform.position;
-                standObj = standPointDown;
-            } else {
-                collision.gameObject.transform.position = standPointUp.transform.position;
-                standObj = standPointUp;
+                Vector3 distUp = other.gameObject.transform.position - standPointUp.transform.position;
+                Vector3 distDown = other.gameObject.transform.position - standPointDown.transform.position;
+                //近い方の待機地点に移動する
+                if (distUp.magnitude > distDown.magnitude) {
+                    other.gameObject.transform.position = standPointDown.transform.position;
+                    standObj = standPointDown;
+                } else {
+                    other.gameObject.transform.position = standPointUp.transform.position;
+                    standObj = standPointUp;
+                }
+                startAngle = this.transform.eulerAngles.z;
+                rotationLate = 0;
+                isRotation = true;
+                isInteractInterval = true;
+                interactIntervalTimer = 0;
             }
-            startAngle = this.transform.eulerAngles.z;
-            rotationLate = 0;
-            isRotation = true;
+
+            if (isRotation == false && isInteractInterval==false) {
+                interact.SetActive(true);
+            } else {
+                interact.SetActive(false);
+                
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            interact.SetActive(false);
         }
     }
 }
--- Assets/Script/Object3D/Gimmick/SecurityDoor.cs
+++ Assets/Script/Object3D/Gimmick/SecurityDoor.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class SecurityDoor : MonoBehaviour {
     [SerializeField] private GameObject point;
     [SerializeField] private GameObject door;
+    [SerializeField] private GameObject interact;
     [SerializeField] private float moveSpeedLate;
-    [SerializeField] private int needKey=1;
+    [SerializeField] private int needKey = 1;
 
     private bool isOpen = false;
+    private bool isInputMouseDown = false;
     private float late;
 
+    private void Start() {
+        interact.SetActive(false);
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            isInputMouseDown = true;
+        }
+    }
 
     private void FixedUpdate() {
         if (isOpen == true) {
@@ -21,6 +32,11 @@ public class SecurityDoor : MonoBehaviour {
         }
     }
 
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            isInputMouseDown = false;
+        }
+    }
 
 
     private void OnTriggerStay(Collider other) {
@@ -28,10 +44,22 @@ public class SecurityDoor : MonoBehaviour {
             if (other.gameObject.CompareTag("Player")) {
                 //ÉJÉMÇéùÇ¡ÇƒÇ¢ÇΩÇÁäJÇ≠
                 Player player = other.gameObject.GetComponent<Player>();
-                if (player.item.GetKey() >= needKey) {
+                if (player.item.GetKey() >= needKey && isInputMouseDown == true) {
                     player.item.UseKey();
+                    interact.SetActive(false);
                     isOpen = true;
+                } else {
+                    interact.SetActive(true);
                 }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (isOpen == false) {
+            if (other.gameObject.CompareTag("Player")) {
+                interact.SetActive(false);
+
             }
         }
     }
