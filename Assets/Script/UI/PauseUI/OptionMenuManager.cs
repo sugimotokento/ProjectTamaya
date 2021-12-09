948d46c2569ef0a987ecfd85e9108201785a9e78
tarnyan <tarnyan15@gmail.com>
2021-12-09 14:31:52 +0900

    現在の進捗 PauseのUI更新

--- Assets/Script/UI/PauseUI/OptionMenuManager.cs
+++ Assets/Script/UI/PauseUI/OptionMenuManager.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class OptionMenuManager : MonoBehaviour
 {
-    // ‰ð‘œ“xButton
-    [SerializeField] GameObject AdjustBigButton;
-    [SerializeField] GameObject AdjustSmallButton;
-
-    // Start is called before the first frame update
     void Start()
     {
-
+        Screen.fullScreen = false;
+        Screen.SetResolution(1280, 720, Screen.fullScreen, 60);
     }
 
     public void OnClickAdjustBigButton()
     {
-        Screen.SetResolution(1920, 1080, false, 60);
+        Screen.SetResolution(1920, 1080, Screen.fullScreen, 60);
     }
 
     public void OnClickAdjustSmallButton()
     {
-        Screen.SetResolution(1280, 720, false, 60);
+        Screen.SetResolution(1280, 720, Screen.fullScreen, 60);
     }
 
+    public void OnClickAdjustFullMode()
+    {
+        Screen.fullScreen = true;
+    }
+
+    public void OnClickAdjustNotFullMode()
+    {
+        Screen.fullScreen = false;
+    }
 }
--- Assets/Script/UI/PauseUI/PauseManager.cs
+++ Assets/Script/UI/PauseUI/PauseManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+// EventTrigger-System使うためのやつ
+using UnityEngine.EventSystems;
+
 
 public class PauseManager : MonoBehaviour
 {
@@ -10,29 +13,50 @@ public class PauseManager : MonoBehaviour
     [SerializeField] GameObject toOptionMenuPanel;
     [SerializeField] GameObject GameEndMenuPanel;
     [SerializeField] GameObject NonePanel;
-    // PauseButton
+    // PauseButton格納用
     [SerializeField] GameObject PauseButton;
+    // EventSystem格納用（どのボタンが押されたかを判定するため）
+    [SerializeField] private EventSystem eventSystem;
     // Panel退避用
     private GameObject nowPanel;
     private GameObject nextPanel;
     // Slide用
-    private float SlideFrame;
-    bool SlideInFlg;
-    bool SlideOutFlg;
-    // SlideFlg取得
+    private int SlideFrame;
+    private bool SlideInFlg;
+    private bool SlideOutFlg;
+    // SlideFlg取得用
     [HideInInspector] public bool isSlideActive = false;
+    // ButtonAnimation用
+    private bool isPushFlg;
+    private bool endPushFlg;
+    private int AnimationFrame;
+    private const float c_ScaleRatio = 0.2f;
+    private const float c_ColorRatio = 0.5f;
+    private GameObject PushButton;
+    private GameObject EndPushButton;
+    // FrameRatio用
+    private const int c_SlideFrameRatio = 10;
+    private const int c_AnimFrameRatio = 5;
 
     void Start()
     {
+        // Panel, SlideIn-Out, ButtonAnimationの初期設定
         nowPanel = nextPanel = NonePanel;
-        SlideFrame = 0.0f;
+        SlideFrame = 0;
         SlideInFlg = SlideOutFlg = isSlideActive = false;
+        isPushFlg = endPushFlg = false;
+        AnimationFrame = 0;
     }
 
     void Update()
     {
+        // PanelSlideIn-Out
         if (nowPanel != nextPanel)
             ChangeMenu();
+        // ButtonAnimation
+        if (isPushFlg || endPushFlg)
+            ButtonAnimation();
+        // Event内外のPauseButton設定
         if (StageManager.instance.isEventActive)
             PauseButton.SetActive(false);
         if (!StageManager.instance.isEventActive)
@@ -74,6 +98,24 @@ public class PauseManager : MonoBehaviour
         nextPanel = GameEndMenuPanel;
     }
 
+    public void OnPushButton()
+    {
+        if (isPushFlg)
+            return;
+
+        isPushFlg = true;
+        AnimationFrame = 0;
+        PushButton = eventSystem.currentSelectedGameObject;
+    }
+
+    public void OnPointerUp()
+    {
+
+        endPushFlg = true;
+        AnimationFrame = c_AnimFrameRatio;
+    }
+
+
     // 本幹 やってることはfadeと同じ
     private void ChangeMenu()
     {
@@ -81,24 +123,24 @@ public class PauseManager : MonoBehaviour
         {
             SlideOutFlg = true;
             isSlideActive = true;
-            SlideFrame = 0.0f;
+            SlideFrame = 0;
         }
 
         if(SlideOutFlg)
         {
             // if you don't need SlideOut or SlideOut is allready finished, please start SlideIn.
-            if(nowPanel == NonePanel || SlideFrame >= 1.0f)
+            if(nowPanel == NonePanel || SlideFrame >= c_SlideFrameRatio)
             {
                 SlideOutFlg = false;
                 SlideInFlg = true;
-                SlideFrame = 0.0f;
+                SlideFrame = 0;
                 nowPanel.SetActive(false);
                 if(nextPanel != NonePanel)
                     nextPanel.SetActive(true);
                 return;
             }
             nowPanel.transform.position -= new Vector3(50.0f, 0.0f, 0.0f);
-            SlideFrame += 0.1f;
+            SlideFrame++;
         }
 
         if (SlideInFlg)
@@ -112,7 +154,7 @@ public class PauseManager : MonoBehaviour
                 nowPanel = nextPanel;
                 return;
             }
-            if (SlideFrame >= 1.0f)
+            if (SlideFrame >= c_SlideFrameRatio)
             {
                 SlideInFlg = false;
                 isSlideActive = false;
@@ -120,8 +162,47 @@ public class PauseManager : MonoBehaviour
                 return;
             }
             nextPanel.transform.position += new Vector3(50.0f, 0.0f, 0.0f);
-            SlideFrame += 0.1f;
+            SlideFrame++;
         }
 
+    }
+
+    private void ButtonAnimation()
+    {
+        if (isPushFlg)
+        {
+            // 縮小
+            float VariableScale = (1.0f/(float)c_AnimFrameRatio) * c_ScaleRatio * AnimationFrame;
+            PushButton.transform.localScale = new Vector3(1.0f - VariableScale, 1.0f - VariableScale, 1.0f);
+            // 明度・暗 (ここのGetComponentだけは許して…)
+            float VariableColor = 1.0f - (1.0f/(float)c_AnimFrameRatio) * c_ColorRatio * AnimationFrame;
+            PushButton.GetComponent<Image>().color = new Color(VariableColor, VariableColor, VariableColor, 1.0f);
+
+            // Animation終了判定
+            if (AnimationFrame >= c_AnimFrameRatio)
+            {
+                EndPushButton = PauseButton;
+                isPushFlg = false;
+            }
+            else
+                AnimationFrame++;
+        }
+
+        if (endPushFlg)
+        {
+            // 拡大(Frameの初期値=FrameRatio)
+            float VariableScale = (1.0f / (float)c_AnimFrameRatio) * c_ScaleRatio * AnimationFrame;
+            PushButton.transform.localScale = new Vector3(1.0f - VariableScale, 1.0f - VariableScale, 1.0f);
+            // 明度・明 (ここのGetComponentだけは許して…)
+            float VariableColor = 1.0f - (1.0f / (float)c_AnimFrameRatio) * c_ColorRatio * AnimationFrame;
+            PushButton.GetComponent<Image>().color = new Color(VariableColor, VariableColor, VariableColor, 1.0f);
+
+            if (AnimationFrame <= 0)
+            {
+                endPushFlg = false;
+            }
+            else
+                AnimationFrame--;
+        }
     }
 }
