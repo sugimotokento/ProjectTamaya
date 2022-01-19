66fab8e80753b825147edb32b06ef04d607ecbae
sugimotokento <keyunutoma@gmail.com>
2022-01-20 00:23:41 +0900

    ゲームオーバーシーンのタイトルに戻る処理を入れ忘れた

--- Assets/Script/Systam/GameOver/GameOverTitleButton.cs
+++ Assets/Script/Systam/GameOver/GameOverTitleButton.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameOverTitleButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] FadeIn fadeIn;
+    [SerializeField] FadeOut fadeOut;
+
+    bool isChangeScene = false;
+
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (isChangeScene == true) {
+            fadeOut.gameObject.SetActive(true);
+            if (fadeOut.GetIsFadeEnd() == true) {
+                SceneManager.LoadScene("Title");
+            }
+        }
+    }
+
+    public void OnClick() {
+        isChangeScene = true;
     }
 }
